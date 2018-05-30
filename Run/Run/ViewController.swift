b2b0b968dc29f8f4a6cdee6b3674916dfa4d0281
Colby Williams <colbylwilliams@gmail.com>
2018-05-30 15:00:07 -0400

    xcode proj creation

---
+++ Run/Run/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Run
+//
+//  Created by Colby L Williams on 5/30/18.
+//  Copyright © 2018 Colby L Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
